355fe25be4bff7ca326145a06be3bd6458464250
Zero <alokil70@gmail.com>
2019-12-08 18:51:50 +0300

    Before connect with backend

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
+import Vuelidate from "vuelidate/src";
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from "./filters/date.filter";
+import messageToastPlugin from "./utils/messageToast.plugin";
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+Vue.use(messageToastPlugin);
+Vue.use(Vuelidate);
+
+Vue.filter('dateFlt', dateFilter);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
